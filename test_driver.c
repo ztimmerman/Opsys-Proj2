929fd76315bd4070c2aa3695526603f9907f8b36
dillonpren <dillonpren@yahoo.com>
2018-03-30 21:18:18 -0400

    BEST TEST

--- test_driver.c
+++ test_driver.c
@@ -1,4 +1,4 @@
-#include <sys/syscall.h>
+#include <unistd.h>
 #define _START 333
 #define _REQUEST 334
 #define _STOP 335
@@ -6,8 +6,14 @@
 int main(){
 
   syscall(333);
-  syscall(334,3,3,3);
-  syscall(335);
+  syscall(334,1,1,6);
+  syscall(334,1,10,1);
+  syscall(334,1,10,1);
+  syscall(334,1,9,1);
+  syscall(334,1,8,1);
+  syscall(334,1,5,1);
+//sleep(20);
+//  syscall(335);
 
   return 0;
 }
